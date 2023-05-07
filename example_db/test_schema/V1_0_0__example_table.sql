eadfbc99c004ba6f87f1dca338ff446038ea8874
andytholmes <andy@andytholmes.co.uk>
2023-05-08 00:03:38 +0100

    updated files to work with changetable on terraform_db. Changes wh and db prefixes

--- example_db/test_schema/V1_0_0__example_table.sql
+++ example_db/test_schema/V1_0_0__example_table.sql
@@ -1,7 +1,6 @@
-use example_db;
+CREATE SCHEMA "test_schema";
 
-
-create or replace TABLE "example_db"."test_schema"."test_table" (
+create or replace TABLE "test_schema"."test_table" (
 	"column1" VARCHAR(100),
 	"column2" NUMBER(38,0),
 	"column3" NUMBER(38,0)
---
+++ example_db/test_schema/V1_0_1__create_test_table2.sql
@@ -0,0 +1,5 @@
+create or replace TABLE "test_schema"."test_table2" (
+	"column1" VARCHAR(100),
+	"column2" NUMBER(38,0),
+	"column3" NUMBER(38,0)
+);
